eb556ec4c0ab95f5594b49baf9eaa4eb6cc35caa
nayan <nayansoni2803@gamail.com>
2024-09-25 11:53:25 +0530

    Initial Commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+      </div>
+      <div className="navbar-left"></div>
+    </div>
+  )
+}
+
+export default Navbar
